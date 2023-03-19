30e19dd0d976946e7849d70bc34972c8360a6a6d
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-03-19 19:03:43 +0500

    Update

---
+++ step00_b/index1.js
@@ -0,0 +1,11 @@
+"use strict";
+let firstName1 = "MyName";
+console.log("My Name : ", firstName1);
+let person = {
+    firstName1: "Shawaal",
+    lastName: "Nadeem",
+    age: 24,
+    city: "Lahore",
+};
+console.log(person);
+console.log(person.city);
