99b15f47c1c9f797432781280084e8156cdc8254
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-03-27 05:12:49 +0500

    calculator

---
+++ calculator/functions.ts
@@ -0,0 +1,16 @@
+const add=(num1:number,num2:number):number=>{
+    return num1+num2;
+}
+
+const sub=(num1:number,num2:number):number=>{
+    return num1-num2;
+}
+
+const mul=(num1:number,num2:number):number=>{
+    return num1*num2;
+}
+
+const div=(num1:number,num2:number):number=>{
+    return num1/num2;
+}
+export default{add,sub,mul,div};
