1a1c2ec7ac7fa4fed20808810df9fcc253987305
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-01 13:07:26 +0500

    static members add in oop

--- oop/index.ts
+++ oop/index.ts
@@ -35,6 +35,33 @@ class Base {
   const b = new Base();
 //   console.log(b.x);           //   error Property 'x' is private and only accessible within class 'Base'.
 
+console.log('\n');
+
+//Static Member
+class user
+{
+  name:string;
+  static id:number=0;
+  constructor(name:string)
+  {
+this.name=name;
+user.id++;                //static members access only by class name.
+  }
+  display()
+  {
+    console.log(this.name);
+console.log(user.id);
+    
+  }
+}
+
+let u1=new user("Nancy");
+u1.display();
+let u2=new user("Shawaal");
+u2.display();
+console.log('\n');
+
+
 class student
 {
    private id:number=0;             
