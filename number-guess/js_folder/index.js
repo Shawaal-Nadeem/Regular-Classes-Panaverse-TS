107fb2d2d0228dad2ba5780c62cab6f175622833
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2023-05-01 18:31:48 +0500

    Number Guessing Game

---
+++ number-guess/js_folder/index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+//User put value
+console.log("Enter any Number Range(1 -> 5)");
+async function userNum() {
+    let answers = await inquirer.prompt([{
+            name: "num",
+            type: "number",
+            message: "Your Turn : "
+        }
+    ]);
+    // Computer put value
+    let range1 = 1;
+    let range2 = 5;
+    let randomNum = Math.floor(Math.random() * (range2 - range1 + 1)) + range1;
+    console.log(`Computer turn : ${randomNum}`);
+    if (answers.num === randomNum) {
+        console.log("Matched");
+    }
+    else {
+        console.log("No Match!!! Try Again");
+        console.log('\n');
+        userNum();
+    }
+}
+userNum();
